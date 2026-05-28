d8ab7ca205406a39439d4f68985ff7bf77c995de
singhdewang007 <singh.dewang.01@gmail.com>
2026-05-28 21:08:52 +0530

    updated theme change

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ele = document.querySelector(".clock p");
-const btn = document.querySelector("#button");
+const btn = document.querySelector("#viewScreen");
 setInterval(() => {
   const date = new Date();
   const inject = date.toLocaleTimeString("en-IN", {
@@ -12,27 +12,57 @@ setInterval(() => {
   ele.innerHTML = inject;
 }, 1000);
 
-
-
 // To check and set the button text based on fullScreenElement Status
-btn.addEventListener("click",()=>{
-    if(document.fullscreenElement){
-        document.exitFullscreen();
-        btn.innerHTML = "View Fullscreen"
-    }
-    else{
-        document.documentElement.requestFullscreen();
-        btn.innerHTML = "Exit Fullscreen"
-    }
-})
-
-// Here fullscreenchange is the Event activity whenever the fullscreenchange state change, this eventlisten run
-document.addEventListener("fullscreenchange",()=>{
-    btn.innerText = document.fullscreenElement? "Exit Fullscreen" : "View Fullscreen";
+btn.addEventListener("click", () => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+    btn.innerHTML = "View Fullscreen";
+  } else {
+    document.documentElement.requestFullscreen();
+    btn.innerHTML = "Exit Fullscreen";
+  }
 });
 
+// Here fullscreenchange is the Event activity whenever the fullscreenchange state change, this eventlisten run
+document.addEventListener("fullscreenchange", () => {
+  ((btn.innerText = document.fullscreenElement
+    ? "Exit Fullscreen"
+    : "View Fullscreen"),
+    (btn.style.opacity = "1"),
+    (theme.style.opacity = "1"));
+});
 
+let timer;
+function resetTimer() {
+  if (document.fullscreenElement) {
+    clearTimeout(timer);
 
+    // show button while moving
+    btn.style.opacity = "1";
+    theme.style.opacity = "1";
 
+    timer = setTimeout(() => {
+      btn.style.opacity = "0";
+      theme.style.opacity = "0";
+    }, 3000);
+  }
+}
 
+// detect cursor movement
+document.addEventListener("mousemove", resetTimer);
 
+// initialize timer
+resetTimer();
+
+// Now let's add the theme change option which consist of dark, and system theme
+const theme = document.querySelector("#theme");
+theme.addEventListener("change", () => {
+  setTheme(theme.value);
+});
+function setTheme(value) {
+  if (value === "darkTheme") {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+}
